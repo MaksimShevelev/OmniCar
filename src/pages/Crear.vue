ff9f8a05455570159eeca53dcda721970bf26718
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-13 14:58:00 -0300

    Update Crear.vue

--- src/pages/Crear.vue
+++ src/pages/Crear.vue
@@ -4,7 +4,7 @@
       <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#239e61] to-[#1d7e4b] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
     <div class="mx-auto max-w-4xl text-center">
-      <h1 class="mt-4 sm:mb-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Crear viaje</h1>
+      <h1 class="mt-4 mb-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Crear viaje</h1>
     </div>
     
     <!-- Grid layout для карточек -->
