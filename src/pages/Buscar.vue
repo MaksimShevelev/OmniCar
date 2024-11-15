fa52ffa83688c587871f73a20234bd58b066bb8f
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-15 19:47:06 +0300

    Update Buscar.vue

--- src/pages/Buscar.vue
+++ src/pages/Buscar.vue
@@ -1,20 +1,19 @@
 <template>
     <div id="map-container" class="relative w-full mb-4 shadow-2xl">
-        <div id="map" class="h-[79vh] w-full rounded-lg"></div>
+        <div id="map" class="h-[75vh] w-full rounded-lg"></div>
         <!-- Контейнер формы, размещённый поверх карты -->
         <div class="form-container absolute bottom-3 right-3 w-80 p-4 shadow-lg rounded-lg bg-white 
     sm:right-2 sm:bottom-2 left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0">
-    <h2 class="text-xl text-green-700 mb-4">Construir ruta</h2>
     <div class="mb-4 suggestions-container">
         <label class="block mb-2"><strong>Origen:</strong></label>
-        <input v-model="origin.city" @input="getSuggestionsDebounced(origin.city, 'origin')" placeholder="Nombre de la ciudad" class="p-2 border rounded mb-2 w-full"/>
+        <input v-model="origin.city" @input="getSuggestionsDebounced(origin.city, 'origin')" class="p-2 border rounded mb-2 w-full"/>
         <ul v-if="originSuggestions.length" class="suggestions-list">
             <li v-for="(suggestion, index) in originSuggestions" :key="index" @click="origin.city = suggestion; originSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
         </ul>
     </div>
     <div class="mb-4 suggestions-container">
         <label class="block mb-2"><strong>Destino:</strong></label>
-        <input v-model="destination.city" @input="getSuggestionsDebounced(destination.city, 'destination')" placeholder="Nombre de la ciudad" class="p-2 border rounded w-full"/>
+        <input v-model="destination.city" @input="getSuggestionsDebounced(destination.city, 'destination')"  class="p-2 border rounded w-full"/>
         <ul v-if="destinationSuggestions.length" class="suggestions-list">
             <li v-for="(suggestion, index) in destinationSuggestions" :key="index" @click="destination.city = suggestion; destinationSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
         </ul>
@@ -209,7 +208,7 @@ export default {
 
 .form-container {
     z-index: 1000;
-    background-color: #ffffff49;
+    background-color: #ffffff91;
 }
 
 .suggestions-list {
