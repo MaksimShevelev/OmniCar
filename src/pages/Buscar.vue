3343ea669c83f9b5ab146d143a29d4d5896b92d9
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-15 17:59:40 +0300

    Update Buscar.vue

--- src/pages/Buscar.vue
+++ src/pages/Buscar.vue
@@ -1,27 +1,29 @@
 <template>
-    <div id="map-container" class="relative h-96 mb-4">
+    <div id="map-container" class="relative w-full mb-4">
         <div id="map" class="h-[79vh] w-full rounded-lg"></div>
         <!-- Контейнер формы, размещённый поверх карты -->
-        <div class="form-container absolute top-4 left-4 w-80 p-4 bg-white shadow-lg rounded-lg">
-            <h2 class="text-xl text-green-700 mb-4">Construir ruta</h2>
-            <div class="mb-4 suggestions-container">
-                <label class="block mb-2"><strong>Origen:</strong></label>
-                <input v-model="origin.city" @input="getSuggestionsDebounced(origin.city, 'origin')" placeholder="Nombre de la ciudad" class="p-2 border rounded mb-2 w-full"/>
-                <ul v-if="originSuggestions.length" class="suggestions-list">
-                    <li v-for="(suggestion, index) in originSuggestions" :key="index" @click="origin.city = suggestion; originSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
-                </ul>
-            </div>
-            <div class="mb-4 suggestions-container">
-                <label class="block mb-2"><strong>Destino:</strong></label>
-                <input v-model="destination.city" @input="getSuggestionsDebounced(destination.city, 'destination')" placeholder="Nombre de la ciudad" class="p-2 border rounded w-full"/>
-                <ul v-if="destinationSuggestions.length" class="suggestions-list">
-                    <li v-for="(suggestion, index) in destinationSuggestions" :key="index" @click="destination.city = suggestion; destinationSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
-                </ul>
-            </div>
-            <button @click="getRoute" class="transition-all py-2 px-4 rounded bg-green-700 text-white focus:bg-green-500 hover:bg-green-500 active:bg-green-900 w-full">
-                Buscar
-            </button>
-        </div>
+        <div class="form-container absolute bottom-3 right-3 w-80 p-4 shadow-lg rounded-lg bg-white 
+    sm:right-2 sm:bottom-2 left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0">
+    <h2 class="text-xl text-green-700 mb-4">Construir ruta</h2>
+    <div class="mb-4 suggestions-container">
+        <label class="block mb-2"><strong>Origen:</strong></label>
+        <input v-model="origin.city" @input="getSuggestionsDebounced(origin.city, 'origin')" placeholder="Nombre de la ciudad" class="p-2 border rounded mb-2 w-full"/>
+        <ul v-if="originSuggestions.length" class="suggestions-list">
+            <li v-for="(suggestion, index) in originSuggestions" :key="index" @click="origin.city = suggestion; originSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
+        </ul>
+    </div>
+    <div class="mb-4 suggestions-container">
+        <label class="block mb-2"><strong>Destino:</strong></label>
+        <input v-model="destination.city" @input="getSuggestionsDebounced(destination.city, 'destination')" placeholder="Nombre de la ciudad" class="p-2 border rounded w-full"/>
+        <ul v-if="destinationSuggestions.length" class="suggestions-list">
+            <li v-for="(suggestion, index) in destinationSuggestions" :key="index" @click="destination.city = suggestion; destinationSuggestions = []" class="suggestion-item">{{ suggestion }}</li>
+        </ul>
+    </div>
+    <button @click="getRoute" class="transition-all py-2 px-4 rounded bg-green-700 text-white focus:bg-green-500 hover:bg-green-500 active:bg-green-900 w-full">
+        Buscar
+    </button>
+</div>
+
     </div>
 </template>
 
@@ -207,6 +209,7 @@ export default {
 
 .form-container {
     z-index: 1000;
+    background-color: #ffffff49;
 }
 
 .suggestions-list {
