1914d007baff9ff7aec4beab23f4494dd9088352
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-22 02:58:56 +0300

    Update MyProfile.vue

--- src/pages/MyProfile.vue
+++ src/pages/MyProfile.vue
@@ -1,134 +1,111 @@
 <template>
-    <div class="space-y-12 mb-40">
-      <!-- Заголовок и ссылки для редактирования -->
-      <div class="flex items-end gap-4 mt-4">
-        <BaseHeading1 class="text-center text-3xl text-gray-900">Mi perfil</BaseHeading1>
-        <router-link to="/mi-perfil/editar" class="mb-6 text-black underline"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg></router-link>
-        <router-link to="/mi-perfil/editar/foto" class="mb-6 text-black underline"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
-  <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-</svg></router-link>
-      </div>
-  
-      <!-- Информация профиля и компоненты -->
-      <section class="border-b border-gray-900/10 pb-12">
-  <p class="mt-1 text-xl text-gray-600">
-    Esta información se mostrará públicamente
-  </p>
-
-  <!-- Данные профиля (отображение) -->
-  <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-    
-    <div class="sm:col-span-4">
-      <label class="block text-sm font-medium text-gray-900">Foto</label>
-      <div class="mt-2 flex justify-start">
-        <ProfileData :user="loggedUser" class="self-start" />
-      </div>
+  <div class="space-y-12 mb-40">
+    <!-- Заголовок и ссылки для редактирования -->
+    <div class="flex items-end gap-4 mt-4">
+      <BaseHeading1 class="text-center text-3xl text-gray-900">Mi perfil</BaseHeading1>
+      <router-link to="/mi-perfil/editar" class="mb-6 text-black underline focus:bg-green-500 hover:bg-green-500 active:bg-green-900">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+        </svg>
+      </router-link>
+      <router-link to="/mi-perfil/editar/foto" class="mb-6 text-black underline focus:bg-green-500 hover:bg-green-500 active:bg-green-900">
+        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+          <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5" />
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+        </svg>
+      </router-link>
     </div>
 
-          <div class="sm:col-span-4">
+    <!-- Информация профиля и автомобиля -->
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <!-- Информация о пользователе -->
+      <section>
+        <h2 class="text-xl font-semibold text-gray-900">Información sobre ti</h2>
+        <div class="mt-6 space-y-4">
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Foto</label>
+            <div class="mt-2 flex justify-start">
+              <ProfileData :user="loggedUser" class="self-start" />
+            </div>
+          </div>
+          <div>
             <label class="block text-sm font-medium text-gray-900">Nombre</label>
             <p class="text-gray-700">{{ loggedUser.displayName || 'Sin especificar' }}</p>
           </div>
-
-          <div class="col-span-full">
+          <div>
             <label class="block text-sm font-medium text-gray-900">Tipo de usuario</label>
-            <p class="text-gray-700 mt-2">{{ loggedUser.rol || 'Sin especificar' }}</p>
+            <p class="text-gray-700">{{ loggedUser.rol || 'Sin especificar' }}</p>
           </div>
-  
-          <div class="col-span-full">
+          <div>
             <label class="block text-sm font-medium text-gray-900">Sobre mí</label>
-            <p class="text-gray-700 mt-2">{{ loggedUser.bio || 'Sin especificar' }}</p>
+            <p class="text-gray-700">{{ loggedUser.bio || 'Sin especificar' }}</p>
           </div>
-
-          <div class="sm:col-span-4">
+          <div>
             <label class="block text-sm font-medium text-gray-900">Email</label>
             <p class="text-gray-700">{{ loggedUser.email || 'Sin especificar' }}</p>
           </div>
-
-
-          <div class="col-span-full">
-  <label for="phone" class="block text-sm/6 font-medium text-gray-900">Número de teléfono</label>
-  <div class="mt-2">
-    <p class="text-gray-700">{{ loggedUser.telefono || 'Sin especificar' }}</p>
-  </div>
-</div>
-
-
-
-          <div><h3>Documentos</h3>
-            <div class="col-span-full">
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Número de teléfono</label>
+            <p class="text-gray-700">{{ loggedUser.telefono || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <h3>Documentos</h3>
             <label for="photo" class="block text-sm/6 font-medium text-gray-900">Foto de DNI</label>
             <div class="mt-2 flex items-center gap-x-3">
               <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" />
-        
             </div>
-          </div>
-
-          <div class="col-span-full">
             <label for="photo" class="block text-sm/6 font-medium text-gray-900">Foto de licencia</label>
             <div class="mt-2 flex items-center gap-x-3">
               <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" />
             </div>
           </div>
-
-          <div class="sm:col-span-2">
+          <div>
             <label class="block text-sm font-medium text-gray-900">Métodos de pago</label>
             <p class="text-gray-700">{{ loggedUser.postalCode || 'Sin especificar' }}</p>
           </div>
-        
-        </div>
         </div>
       </section>
-  
-      <section class="border-b border-gray-900/10 pb-12">
-  <h2 class="text-xl font-semibold text-gray-900">Información sobre auto</h2>
 
-  <div class="mt-10 space-y-8">
-    <div>
-      <label for="photo" class="block text-sm font-medium text-gray-900">Foto</label>
-      <div class="mt-2 flex items-center gap-x-3">
-        <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" />
-      </div>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Marca</label>
-      <p class="text-gray-700">{{ loggedUser.firstName || 'Sin especificar' }}</p>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Modelo</label>
-      <p class="text-gray-700">{{ loggedUser.lastName || 'Sin especificar' }}</p>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Año</label>
-      <p class="text-gray-700">{{ loggedUser.country || 'Sin especificar' }}</p>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Color</label>
-      <p class="text-gray-700">{{ loggedUser.streetAddress || 'Sin especificar' }}</p>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Tipo de combustible</label>
-      <p class="text-gray-700">{{ loggedUser.city || 'Sin especificar' }}</p>
-    </div>
-
-    <div>
-      <label class="block text-sm font-medium text-gray-900">Consumo promedio</label>
-      <p class="text-gray-700">{{ loggedUser.region || 'Sin especificar' }}</p>
+      <!-- Информация об автомобиле -->
+      <section>
+        <h2 class="text-xl font-semibold text-gray-900">Información sobre tu auto</h2>
+        <div class="mt-6 space-y-4">
+          <div>
+            <label for="photo" class="block text-sm font-medium text-gray-900">Foto</label>
+            <div class="mt-2 flex items-center gap-x-3">
+              <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" />
+            </div>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Marca</label>
+            <p class="text-gray-700">{{ loggedUser.firstName || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Modelo</label>
+            <p class="text-gray-700">{{ loggedUser.lastName || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Año</label>
+            <p class="text-gray-700">{{ loggedUser.country || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Color</label>
+            <p class="text-gray-700">{{ loggedUser.streetAddress || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Tipo de combustible</label>
+            <p class="text-gray-700">{{ loggedUser.city || 'Sin especificar' }}</p>
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-900">Consumo promedio</label>
+            <p class="text-gray-700">{{ loggedUser.region || 'Sin especificar' }}</p>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
-</section>
-
-    </div>
-  </template>
+</template>
   
   <script setup>
   import { ref, onMounted, onUnmounted } from 'vue';
