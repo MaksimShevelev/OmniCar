aafc498a6d9705d6293a17ad846f21fe78afb837
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-22 02:32:01 +0300

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,17 @@
 <template>
 
     <div class="mx-auto py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div
-            class="relative isolate overflow-hidden bg-gray-100 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 flex flex-col lg:flex-row">
+        <div class="relative isolate overflow-hidden bg-gray-100 px-6 pt-16 shadow-2xl rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 flex flex-col lg:flex-row">
+            <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+                aria-hidden="true">
+                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#239e61] to-[#1d7e4b] opacity-20"
+                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+            </div>
+            <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+                aria-hidden="true">
+                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#239e61] to-[#1d7e4b] opacity-20"
+                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+            </div>
             <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-24 lg:text-left">
                 <h1 class="text-balance text-6xl font-semibold tracking-tight text-green-700">
                     ¡Bienvenido a bordo de OmniCar!</h1>
@@ -24,8 +33,7 @@
     </div>
     
     <div class="mx-auto py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div
-            class="relative mx-auto max-w-7xl isolate overflow-hidden py-24 sm:py-32 bg-white sm:py-20 px-6 lg:px-8 rounded-3xl shadow-lg mb-40 flex justify-center items-center">
+        <div class="relative bg-gray-100 mx-auto max-w-7xl isolate overflow-hidden py-16 sm:py-24 bg-white sm:py-20 px-6 lg:px-8 rounded-3xl shadow-2xl mb-40 flex justify-center items-center">
             <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
                 aria-hidden="true">
                 <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#239e61] to-[#1d7e4b] opacity-20"
