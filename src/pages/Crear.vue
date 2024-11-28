df707e96d34bdffde682f5969e1576906c56cd50
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-28 14:32:41 +0300

    Update Crear.vue

--- src/pages/Crear.vue
+++ src/pages/Crear.vue
@@ -4,10 +4,10 @@
       <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#239e61] to-[#1d7e4b] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
     <div class="mx-auto max-w-4xl text-center">
-      <h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Crear viaje</h1>
+      <h1 class="mt-4 text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Crear viaje</h1>
     </div>
     
-    <!-- Grid layout для карточек -->
+
     <div class="mx-auto mt-4 grid max-w-lg gap-8 sm:gap-10 lg:max-w-4xl lg:grid-cols-2">
       <div
         v-for="(tier, index) in tiers"
@@ -21,7 +21,7 @@
       >
         <div class="relative sm:mb-4 mt-4">
           <h2 :id="tier.id" class="text-green-700 text-base font-semibold">{{ tier.name }}</h2>
-          <img :src="tier.image" alt="Описание изображения" class="mt-4 w-full h-auto rounded-md">
+          <img :src="tier.image" alt="Descripción de la imagen" class="mt-4 w-full h-auto rounded-md">
           
           <ul class="mt-8 space-y-3 text-sm text-green-700 sm:mt-10">
             <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
@@ -57,14 +57,14 @@
 import { ref } from 'vue'
 import { CheckIcon } from '@heroicons/vue/20/solid'
 
-const hoveredIndex = ref(null) // индекс для отслеживания карточки под мышью
+const hoveredIndex = ref(null) 
 
 const tiers = [
   {
     name: 'Para pasajeros',
     id: 'tier-hobby',
     href: '#',
-    image: 'https://firebasestorage.googleapis.com/v0/b/omnicar-80e07.firebasestorage.app/o/12.jpg?alt=media&token=db1659ae-2967-44a3-af52-53abfd4d56de',
+    image: '../public/12.jpg',
     features: [
       'Conductores verificados',
       'Creación de solicitudes para eventos',
@@ -78,7 +78,7 @@ const tiers = [
     name: 'Para conductores',
     id: 'tier-enterprise',
     href: '#',
-    image: 'https://firebasestorage.googleapis.com/v0/b/omnicar-80e07.firebasestorage.app/o/10.jpg?alt=media&token=e71d8570-b081-46b4-85e0-10fbaedbdc5b',
+    image: '../public/10.jpg',
     features: [
       'Pasajeros verificados',
       'Ahorro en combustible y peajes',
