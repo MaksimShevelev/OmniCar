3a01ff885d5236e35a9caa57f399c96cacf78840
MaksimShevelev <109753407+MaksimShevelev@users.noreply.github.com>
2024-11-28 14:33:55 +0300

    Update MyProfile.vue

--- src/pages/MyProfile.vue
+++ src/pages/MyProfile.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="space-y-12 mb-40 ">
-    <!-- Заголовок и ссылки для редактирования -->
     <div class="flex items-end gap-4 mt-4">
       <BaseHeading1 class="text-center text-3xl text-gray-900">Mi perfil</BaseHeading1>
       <router-link to="/mi-perfil/editar" class="mb-6 text-gray-900 underline focus:text-green-700 hover:text-green-700 active:text-green-700 ">
@@ -17,10 +16,8 @@
       </router-link>
     </div>
 
-    <!-- Информация профиля и автомобиля -->
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-8" style="
     margin-top: 2px">
-      <!-- Информация о пользователе -->
       <section>
         <h2 class="inline-block bg-gray-100 text-xl p-2 border rounded-xl shadow text-gray-700 font-semibold">Información sobre ti</h2>
         <div class="mt-6 space-y-4">
@@ -68,7 +65,6 @@
         </div>
       </section>
 
-      <!-- Информация об автомобиле -->
       <section>
         <h2 class="inline-block bg-gray-100 text-xl p-2 border rounded-xl shadow text-gray-700 font-semibold">Información sobre tu auto</h2>
         <div class="mt-6 space-y-4">
